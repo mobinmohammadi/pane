6e8d07bc19482c9f2104b268cf336ec2c9fab99a
mobin <mobinmohammadi4321@gmail.com>
2025-06-11 20:46:04 +0330

    fkjvdffs

--- src/assets/Components/FormComponent/FormComponent.tsx
+++ src/assets/Components/FormComponent/FormComponent.tsx
@@ -1,52 +1,104 @@
+import { useFormik } from "formik";
 import React from "react";
+import { toast } from "react-toastify";
+
+type InitailType = {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+};
 
 export default function FormComponent() {
+  const addNewUserHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    toast.success("عملیات با موفقیت انجام شد!");
+    setTimeout(() => {
+      location.reload()
+      
+    }, 2000);
+  };
+
+  const formik = useFormik<InitailType>({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      phone: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className="bg-[#212d45] w-full rounded-sm font-Dana-Demi-bold p-5">
-      <form action="#">
-        <div className="grid grid-cols-1 xs:grid-cols-2 gap-3">
-          <div className="text-dark-menu flex flex-col gap-2">
-            <label className="text-sm" htmlFor="name">
-              نام و نام خانوادگی
-            </label>
-            <input
-              className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
-              type="text"
-              placeholder="نام و نام خانوادگی ..."
-            />
+      <form action="#" onSubmit={formik.handleSubmit}>
+        <div className="flex gap-5 flex-col">
+          <div className="grid grid-cols-1 xs:grid-cols-2 gap-3">
+            <div className="text-dark-menu flex flex-col gap-2">
+              <label className="text-sm" htmlFor="name">
+                نام و نام خانوادگی
+              </label>
+              <input
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
+                type="text"
+                placeholder="نام و نام خانوادگی ..."
+              />
+            </div>
+            <div className="text-dark-menu flex flex-col gap-2">
+              <label className="text-sm" htmlFor="name">
+                پسوورد
+              </label>
+              <input
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
+                type="text"
+                placeholder="پسوورد..."
+              />
+            </div>
+            <div className="text-dark-menu flex flex-col gap-2">
+              <label className="text-sm" htmlFor="name">
+                شماره تماش
+              </label>
+              <input
+                name="phone"
+                value={formik.values.phone}
+                onChange={formik.handleChange}
+                className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
+                type="text"
+                placeholder="شماره تماس..."
+              />
+            </div>
+            <div className="text-dark-menu flex flex-col gap-2">
+              <label className="text-sm" htmlFor="name">
+                ایمیل
+              </label>
+              <input
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
+                type="text"
+                placeholder="ایمیل ..."
+              />
+            </div>
           </div>
-          <div className="text-dark-menu flex flex-col gap-2">
-            <label className="text-sm" htmlFor="name">
-              پسوورد
-            </label>
-            <input
-              className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
-              type="text"
-              placeholder="پسوورد..."
-            />
-          </div>
-          <div className="text-dark-menu flex flex-col gap-2">
-            <label className="text-sm" htmlFor="name">
-              شماره تماش
-            </label>
-            <input
-              className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
-              type="text"
-              placeholder="شماره تماس..."
-            />
-          </div>
-          <div className="text-dark-menu flex flex-col gap-2">
-            <label className="text-sm" htmlFor="name">
-              ایمیل
-            </label>
-            <input
-              className="text-xs outline-0 bg-slate-400 text-zinc-900 pt-2 pb-2 pr-2 rounded-sm"
-              type="text"
-              placeholder="ایمیل ..."
-            />
+          <div className="w-full flex justify-end text-white  text-sm ">
+            <button
+            type="submit"
+              onClick={(e) => addNewUserHandler(e)}
+              className="bg-zinc-800 rounded-md hover:bg-zinc-900 transition-all cursor-pointer w-22 pt-2 pb-2"
+            >
+              ثبت کاربر
+            </button>
           </div>
         </div>
-        <button className=""></button>
       </form>
     </div>
   );
--- src/assets/Pages/Users/Users.tsx
+++ src/assets/Pages/Users/Users.tsx
@@ -1,10 +1,25 @@
 import FormComponent from "@/assets/Components/FormComponent/FormComponent"
 import TableDataCustom from "@/assets/Components/TableDataCustom/TableDataCustom"
+import { ToastContainer } from "react-toastify"
 export default function Users() {
   return (
     <div className="flex fl-col gap-5 flex-col">
       <FormComponent/>
       <TableDataCustom/>
+       <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={true} // برای زبان فارسی
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+          toastClassName="custom-toast"
+
+        theme="colored"
+      />
     </div>
   )
 }
