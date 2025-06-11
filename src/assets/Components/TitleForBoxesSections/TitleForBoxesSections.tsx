1f6f23d0d7bfddf699982078c4a4da4f48bbe493
mobin <mobinmohammadi4321@gmail.com>
2025-06-11 18:13:01 +0330

    fkjvsdfn

--- src/assets/Components/TitleForBoxesSections/TitleForBoxesSections.tsx
+++ src/assets/Components/TitleForBoxesSections/TitleForBoxesSections.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function TitleForBoxesSections({title} : Props) {
   return (
-    <div className='relative mb-7 flex items-center mt-7'>
+    <div className='relative mt-5 mb-5 sm:mb-7 flex items-center  sm:mt-7'>
         <span className='font-Dana-Demi-bold text-sm sm:text-sm dark:text-white text-zinc-900 mr-4 font-bold'>{title}</span>
         <span className='absolute bg-yellow-500 w-3 h-3 right-0 top-1 rounded-sm'> </span>
     </div>
